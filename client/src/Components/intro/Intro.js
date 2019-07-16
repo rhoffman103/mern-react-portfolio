3a6ccd91c1681a0af8f9f74ddc58c9b4ac58864e
Bobby H <rhoffman103@gmail.com>
2019-07-16 15:43:11 -0400

    button focus style

--- client/src/Components/intro/Intro.js
+++ client/src/Components/intro/Intro.js
@@ -8,8 +8,13 @@ const Intro = (props) => (
                 <div className="flex item">
                     <p>Hello, I'm <span className=" font-weight-bold calus-purple-text">Bobby Hoffman</span>.</p>
                     <p>A full-stack web developer graduate from the UNH Coding Bootcamp.</p>
-                    <button type="button" onClick={props.smoothScroll} className="btn transparent-btn btn-border-gold calus-gold-text btn-lg check-work-btn" data-scroll-to="about-me">Check Out My Work</button>
-                    {/* onClick="document.getElementById('about-me').scrollIntoView();" */}
+                    <button
+                        type="button" onClick={props.smoothScroll}
+                        className="btn transparent-btn btn-border-gold calus-gold-text btn-lg check-work-btn"
+                        data-scroll-to="about-me"
+                    >
+                        Check Out My Work
+                    </button>
                 </div>
             </div>
         </div>
