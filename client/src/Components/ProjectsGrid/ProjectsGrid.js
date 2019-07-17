92b45fd3889faf2ab852950da91d31ee1df0ced5
Bobby H <rhoffman103@gmail.com>
2019-07-16 21:50:24 -0400

    refactored projects grid

--- client/src/Components/ProjectsGrid/ProjectsGrid.js
+++ client/src/Components/ProjectsGrid/ProjectsGrid.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import ProjectList from '../ProjectList';
+import MainJS from '../../utils/main';
 import './projectsGrid.css';
 
-const ProjectsGrid = ({ projects }) => (
-    <div className="container ptb-100" id="portfolio">
-        {projects ?
-            <React.Fragment>
-                <div className="wrapper">
-                    <div className="portfolio">
-                        <h2>Portfolio</h2>
-                    </div>
-                </div>
-                <div className="projects">
-                    <ProjectList projects={projects}/>
-                </div>
-            </React.Fragment>
-        :
-        <div>something went wrong fetching projects</div>
+class ProjectsGrid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mongoProjects: null
         }
-    </div>
-)
+    };
+
+    componentDidMount() {
+        MainJS.getProjects(this);
+    };
+
+    render() {
+        return (
+            <div className="container ptb-100" id="portfolio">
+                {this.state.mongoProjects ?
+                    <React.Fragment>
+                        <div className="wrapper">
+                            <div className="portfolio">
+                                <h2>Portfolio</h2>
+                            </div>
+                        </div>
+                        <div className="projects">
+                            <ProjectList projects={this.state.mongoProjects}/>
+                        </div>
+                    </React.Fragment>
+                :
+                <div>something went wrong fetching projects</div>
+                }
+            </div>
+        )
+    };
+};
 
 export default ProjectsGrid;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,29 +3,21 @@ import Intro from '../../Components/intro';
 import AboutMe from '../../Components/About';
 import ProjectsGrid from '../../Components/ProjectsGrid';
 import Footer from '../../Components/Footer';
-import MainJS from '../../utils/main';
 import ReactGA from 'react-ga';
 import MyNav from '../../Components/Nav/MyNav';
 
 import '../../App.css';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mongoProjects: null
-        }
-    }
 
     componentDidMount() {
         this.initializeReactGA();
-        MainJS.getProjects(this);
-    }
+    };
 
     initializeReactGA() {
         ReactGA.initialize('UA-131375937-1');
         ReactGA.pageview('/');
-    }
+    };
 
     render() {
         return (
@@ -33,11 +25,11 @@ class Home extends Component {
                 <Intro />
                 <MyNav homePage={true} />
                 <AboutMe />
-                <ProjectsGrid projects={this.state.mongoProjects} />
+                <ProjectsGrid />
                 <Footer />
             </React.Fragment>
         )
-    }
-}
+    };
+};
 
 export default Home;
