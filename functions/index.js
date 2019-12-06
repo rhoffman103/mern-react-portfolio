6accfff8a5e9a22f2f16dba8955164028bab2aab
Bobby H <rhoffman103@gmail.com>
2019-12-06 13:18:03 -0500

    firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,14 @@
 require ('dotenv').config();
-
+const functions = require('firebase-functions');
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 const app = express();
 
 // configure middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 // Routes
 const apiRoutes = require('./routes/apiRoutes');
@@ -29,12 +23,4 @@ mongoose.Promise = Promise;
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
-});
+exports.app = functions.https.onRequest(app);
